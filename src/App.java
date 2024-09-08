bfd998b3dc25947f508ef2ae80038215b7fbb1a7
wignn <tigfiragnafatur1933@gmail.com>
2024-09-08 13:44:51 +0700

    dean

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        try {
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
